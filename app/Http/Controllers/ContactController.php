5108171c1f1bff9e25d34dbcf19efcb04e5dc519
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-02-22 09:16:05 +0100

    Added make:mail Class for sending HTML mails through blade view

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,7 +1,8 @@
 <?php
     namespace App\Http\Controllers;
     use Illuminate\Support\Facades\Mail;
-    
+    use App\Mail\ContactMe;
+
     class ContactController extends Controller{
         
         public function show(){
@@ -14,10 +15,15 @@
             //Backend email verification
             request()->validate(['email' => 'required|email']);
 
-            Mail::raw('It sends email!', function ($message){
-                $message->to(request('email'))
-                    ->subject('HelloThere');
-            });
+            #Primer método de envío
+            // Mail::raw('It sends email!', function ($message){
+            //     $message->to(request('email'))
+            //         ->subject('HelloThere');
+            // });
+
+            #Segundo método de envío a través de plantilla Blade (email en HTML)
+            Mail::to(request('email'))
+                ->send(new ContactMe());
 
             return redirect('/contact')
             ->with('message', 'Email enviado!');

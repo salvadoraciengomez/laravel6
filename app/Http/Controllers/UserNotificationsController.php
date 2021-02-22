97ce70334bfed858c8cdcb7a255b42165c92ecff
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-02-22 19:22:20 +0100

    Notification markAsRead II

--- app/Http/Controllers/UserNotificationsController.php
+++ app/Http/Controllers/UserNotificationsController.php
@@ -9,12 +9,8 @@ class UserNotificationsController extends Controller
     
     public function show(){
         
-        $notifications= auth()->user()->unreadNotifications;
-
-        $notifications->markAsRead();
-        
         return view('notifications.show', [
-            'notifications' => $notifications
+            'notifications' => tap(auth()->user()->unreadNotifications)->markAsRead()
         ]);
     }
 }

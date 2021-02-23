26feda82b06bd1feb52e3088e3cefdd9f92e5a3f
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-02-23 14:25:45 +0100

    Role & Ability Model allow mass asignment

--- app/Ability.php
+++ app/Ability.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ability extends Model
 {
+    protected $guarded=[];
     public function roles(){
         return $this->belongsToMany(Role::class)->withTimestamps();
     }
--- app/Role.php
+++ app/Role.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    protected $guarded=[];
     public function abilities(){
         return belongsToMany(Ability::class)->wiithTimestamps();
     }
